97a6218f815dfa6039dd6bc54bf5f61d29987be8
Indah Nuur Hasanah <54131090+indahnuur@users.noreply.github.com>
2019-09-08 22:08:12 +0700

    pertememuan ke-2

---
+++ Pertemuan ke 2.rb
@@ -0,0 +1,87 @@
+ ke 21
+
+def salkem
+puts "Hai salam kenal"
+end
+
+Hai salam kenal
+Hai Salam kenal
+Hai salam kenal
+
+22
+
+def salkem
+puts "Hai salam kenal"
+enddef salkem
+puts "Hai salam kenal"
+end
+
+3.times {print salkem}
+
+
+23
+
+def salkem(kata)
+	puts "Hai salam kenal nama saya #{kata}"
+end
+
+salkem('Made')
+
+24
+def salkem(kata,koto)
+puts "Hai salam kenal nama saya #{kata} dan #{koto}"
+end
+
+salkem('Made', 'Bambang')
+
+
+25
+def salkem(kata,koto,kutu)
+puts "Hai salam kenal nama saya #{kata} dan #{koto}, aku benci #{kutu}"
+end
+
+salkem('Made', 'Bambang', 'Rio')
+
+26
+def pengurangan(angkasatu, angkadua)
+angkasatu - angkadua
+end
+
+kurang = pengurangan(5, 3)
+puts kurang
+
+
+27
+def salkem (nama)
+puts "Hai salam kenal nama saya #{nama}"
+end
+salkem("A")
+salkem("B")
+salkem("C")
+
+28
+def penjumlahan(nilai_a, nilai_b)
+nilai
+
+belum kelar
+
+
+29
+def penjumlahan(nilai_a, nilai_b)
+return nilai_a - nilai_b if nilai_a ==5
+nilai_a + nilai_b
+nilai_a / nilai_b if nilai_b <=3
+belum kelar ngetik
+
+
+
+30
+
+def halo
+"Saya sedang belajar ruby"
+end
+
+hasil = halo
+puts hasil
+puts hasil.gsub{'sedang','sudah')
+puts .reverse
